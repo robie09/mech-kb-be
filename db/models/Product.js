cb216c0946a27d4166a998d0b9f6d0da7ebf8c88
DarthHamza <hamzamakia@gmail.com>
2021-02-08 16:38:53 +0300

    linked to sequelize

---
+++ db/models/Product.js
@@ -0,0 +1,25 @@
+const SequelizeSlugify = require("sequelize-slugify");
+
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    slug: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  });
+  SequelizeSlugify.slugifyModel(Product, {
+    source: ["name"],
+  });
+  return Product;
+};
